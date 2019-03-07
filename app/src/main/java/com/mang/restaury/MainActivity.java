8360005bc69b392c1755f82d2a769d39d26e614f
Sarayut <imkopkap@gmail.com>
2019-03-07 22:34:56 +0700

    first commit

---
+++ app/src/main/java/com/mang/restaury/MainActivity.java
@@ -0,0 +1,57 @@
+package com.mang.restaury;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.mang.restaury.Fragments.FavoriteFragment;
+import com.mang.restaury.Fragments.SearchFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment selectedFragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.search_page:
+                    selectedFragment = new SearchFragment();
+                    break;
+
+                case R.id.favorite_page:
+                    selectedFragment = new FavoriteFragment();
+                    break;
+            }
+
+            renderFragment(selectedFragment);
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        selectedFragment = new SearchFragment();
+        renderFragment(selectedFragment);
+
+    }
+
+    private void renderFragment(Fragment selectedFragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.wrapper, selectedFragment)
+                .commit();
+    }
+
+}
