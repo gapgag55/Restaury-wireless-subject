1027591a456d5a517bc53c2e85caeb6b80ac485e
Sarayut <imkopkap@gmail.com>
2019-03-22 19:49:18 +0700

    Update Restaurant Activity

--- app/src/main/java/com/mang/restaury/Fragments/AboutFragment.java
+++ app/src/main/java/com/mang/restaury/Fragments/AboutFragment.java
@@ -1,8 +1,7 @@
 package com.mang.restaury.Fragments;
 
-
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +11,7 @@ import com.mang.restaury.R;
 /**
  * A simple {@link Fragment} subclass.
  */
+
 public class AboutFragment extends Fragment {
 
 
--- app/src/main/java/com/mang/restaury/RestaurantActivity.java
+++ app/src/main/java/com/mang/restaury/RestaurantActivity.java
@@ -1,12 +1,25 @@
 package com.mang.restaury;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.mang.restaury.Adapter.ResturantTabAdapter;
+import com.mang.restaury.Fragments.AboutFragment;
+import com.mang.restaury.Fragments.MenusFragment;
+import com.mang.restaury.Fragments.ReviewsFragment;
+
 public class RestaurantActivity extends AppCompatActivity {
 
     private TextView toolbar_title;
+
+    private static final String TAG = "MainActivity";
+
+    private ResturantTabAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +30,22 @@ public class RestaurantActivity extends AppCompatActivity {
 
         toolbar_title = (TextView) findViewById(R.id.toolbar_title);
         toolbar_title.setText(restaurant_name);
+
+        mSectionsPageAdapter = new ResturantTabAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ResturantTabAdapter adapter = new ResturantTabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MenusFragment(), "Menus");
+        adapter.addFragment(new AboutFragment(), "About");
+        adapter.addFragment(new ReviewsFragment(), "Reviews");
+        viewPager.setAdapter(adapter);
     }
 }
